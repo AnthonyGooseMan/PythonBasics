cb5fca944f90df0d82821ad232d9ed9b0e67715f
Guzman <aguzman134@cps.k12.il.us>
2024-04-29 08:40:43 -0500

    learning basics

---
+++ day3.py
@@ -0,0 +1,28 @@
+# Booleans
+print(10>9)  # is this true or false
+# Booleans are used to evaluate exppresions and conditions in python
+# They are used to check if an expression is true or not
+broughtFood=True
+print(broughtFood)
+is_raining = True
+if is_raining:
+    print("Take an Unbrella!")
+else:
+    print("No umbrella needed")
+temperature = 15
+if temperature > 30: 
+    print("Its warm")
+    print("Drink Water")
+elif temperature > 20:
+    print("Its' nice")
+else:
+    print("It's cold")
+print("Done")
+
+age=22
+if age >= 18:
+      message="Eligable"
+else:
+      message= "Not Eligible"
+
+      print(message)
---
+++ main.py
@@ -0,0 +1,24 @@
+print("hello world")
+# print is a function that prints out a message to console
+# Strings are surrounded by quotes
+# Single or double quotes " or '
+# Whenever words are surrounded by quotes it is called a string
+# Be consistent with the quotes you use 
+print('Anthony Guzman')
+print('order of execution')
+print('in python')
+print('*'*20)
+price=10
+name='John'
+rating= 4.9
+is_published=True
+print('name')
+print('price')
+print('rating')
+print("Leglorious king")
+print(name + " has a rating of" + str(rating))
+# input from the actual user
+name = input('Anthony Guzman')
+age = input("15")
+Occupation = input('school')
+print('hello' + name + 'you are' + age + 'years old and you do' + Occupation)
