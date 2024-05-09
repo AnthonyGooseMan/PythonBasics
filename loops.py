9fffd3141c4663e2b195648255e32dbf3a90f2bf
Guzman <aguzman134@cps.k12.il.us>
2024-05-09 08:38:22 -0500

    loop practice

---
+++ loops.py
@@ -0,0 +1,30 @@
+nums = range (1-10000)
+
+# for num in nums:
+#     for lenumsrxirctter in 'abc':
+#         for letter2 in 'abc':
+#             for letter3 in 'abc':
+#                 for letter4 in 'abc':
+#                    print(num, letter, letter2, letter3, letter4)
+# # while loop
+# # when you do not know the number of iteration
+# # for i in range(250):
+# #     print(i)
+
+# #
+
+# while True:
+#     print("Enter your name:")
+#     namenput()
+
+# loops repeat a b;ock of code multiple times 
+# Whole loop repeats a block of codse as long as the conditions are met
+# For  loops repeats a block of code a specific number of times 
+list1= range(1,1001)
+print(list1)
+for number in list1:
+    if number % 2 == 0:
+        print(f'number is even (number)')
+    else:
+         print("This number is not even")
+print(f'the sum of the list is {sum(list1)}')
---
+++ whileReview.py
@@ -0,0 +1,21 @@
+# While loops allows you to repeat code until a condition is met
+# While loops are defined with the while keyboard
+# While loo[s are use when you don't knbow how many times you neeed to repeat the code
+
+# studentList = [ "Citlali", "Praise", "Aaron","Fiona", "Xavier"]
+# studentName = 0
+# while studentName < len(studentList):
+#     print(studentList[studentName])
+#     studentName                                                                                                                                                              += 1
+
+# fruitList = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+# # for loop to print the fruitList
+# # for loop is when you know how many times 
+# # you need to repeat the code
+# for fruit in fruitList:
+#     print(fruit + "is a fruit")
+
+# studentList = [ "Citlali", "Praise", "Aaron","Fiona", "Xavier"]
+# for student in studentList:
+#     ask = input("What is your last name?")
+#     print(student + "" + ask)
